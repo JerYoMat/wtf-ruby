136c1d94b8f65105a8f4e2afbb6600a74d61ddac
JerYoMat <jeremy.emata@gmail.com>
2018-12-24 17:07:51 -0500

    tidy up before gem attempt

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,17 +8,15 @@ class CommandLineInteface
       @user_input = ' '
     end
 
+
     def run
       #setup
       base_setup
-      puts "Please see below for possible actions:".colorize(:mode => :underline)
       until @user_input.downcase == 'exit'
-
-
         prompt_text
         @user_input = gets.strip
         validate_choice_input(@user_input) unless @user_input == ''
-
+  #User has not chosen a valid class through their first selection and should see a list of all classes
         if @current_class == nil && @user_input != "exit"
           @user_input = ' '
           until @current_class != nil
@@ -27,34 +25,29 @@ class CommandLineInteface
             validate_choice_input(@user_input)
           end
         end
+  #User has chosen a class but no method
+  #The first if statement is for classes that has methods.
         if @current_class && @current_class.has_methods? && @current_method == nil
           @user_input = ''
-          until @current_method != nil
+          until @current_method != nil || @user_input == 'exit'
             puts ''
             printf_method_list(@current_class)
-            puts 'Enter the name of the method or exit to quit'
             @user_input = gets.strip
             @current_method = set_method(@current_class,@user_input) if set_method(@current_class,@user_input)
           end
+  #The selected class does not have methods.
         elsif @current_class && !@current_class.has_methods?
-          3.times do
-            printf("\n")
-          end
-          printf("\t%s does not have any methods.\n".colorize(:color => :cyan), @current_class.colorize(:color => :red))
-          printf("\tPlease hold ctrl and click the below link for additional information.\n")
-          printf("\t#{@current_class.source.colorize(:mode => :bold)}\n")
-          3.times do
-            printf("\n")
-          end
+           print_class_with_no_method_content
         end
+  #User has selected a class and method
           if @current_class && @current_class.has_methods? && !@current_method.nil?
             display_method(@current_class, @current_method)
-            sleep(4)
           end
+  #Reset the class or alternately instantiate a new CLI instance.
         @current_class = nil
         @current_method = nil
-
-    end
+          sleep(3)
+      end
     end
 
 
@@ -65,17 +58,37 @@ class CommandLineInteface
     Classy.create_methods_for_all_classes
   end
 
+  def make_classes
+    Classy.create_from_collection(Scraper.scrape_class)
+  end
 
 
 
+  def print_class_with_no_method_content
+    3.times do
+      printf("\n")
+    end
+    printf("\t%s does not have any methods.\n".colorize(:color => :cyan), @current_class.name.colorize(:color => :red))
+    printf("\tPlease hold ctrl and click the below link for additional information.\n")
+    printf("\t#{@current_class.source.colorize(:mode => :bold)}\n")
+    3.times do
+      printf("\n")
+    end
+
+  end
 
 
+  def prompt_text
+      enter = bold_and_green("ENTER")
+    c_name = bold_and_green("Class_name")
+    m_name = bold_and_green("method_name")
+    puts "For a list of all available classes: press #{enter}"
+    puts "For a list of methods of a particular class: #{c_name}"
+    puts "To see description and sample code for a specific class and method:"
+    puts "#{c_name}, #{m_name}"
+  end
 
-#Reqs
-#If given a string, this method should determine whether the string name matches an individual class,
-#an individual class and method
-#If valid class is received then set the class
-#If valide method is received then set the method.
+
   def isolate_inputs(str)
     str.scan(/\w+/)
   end
@@ -87,23 +100,8 @@ class CommandLineInteface
       if user_inputs.count == 2 && set_method(@current_class, user_inputs[1])
         @current_method = set_method(@current_class, user_inputs[1])
       end
-
     end
   end
-
-
-
-
-  def prompt_text
-      enter = bold_and_red("ENTER")
-    c_name = bold_and_red("Class_name")
-    m_name = bold_and_red("method_name")
-    puts "  For a list of all available classes: press #{enter}"
-    puts "  For a list of methods of a particular class: #{c_name}"
-    puts "  To see description and sample code for a specific class and method:"
-    puts "  #{c_name}, #{m_name}"
-  end
-
 
 
 
@@ -112,19 +110,12 @@ class CommandLineInteface
     class_instance.meth_ods.select { |m| m.name == chosen_method_name.downcase}.first
   end
 
-  def make_classes
-    Classy.create_from_collection(Scraper.scrape_class)
-  end
-
   def set_class(string)
     Classy.all.select { |c| c.name.upcase == string.upcase}.first
   end
 
 
-
-
   def display_method(class_instance, chosen_method)
-
       m_to_show = set_method(class_instance, chosen_method.name)
       puts ''
       puts '  Class: ' + class_instance.name.colorize(:mode => :bold) + ' Method: ' + m_to_show.name.colorize(:color => :red, :mode => :bold)
@@ -138,6 +129,10 @@ class CommandLineInteface
       end
       puts ''
       puts "Source: #{class_instance.source}"
+      3.times do
+        printf("\n")
+      end
+
   end
 
 
@@ -149,15 +144,16 @@ class CommandLineInteface
       rows = selected_class.meth_ods.count.to_f / 3.00
       remainder = selected_class.meth_ods.count % 3
       rows.floor.times do
-        printf(" %2d.%-25s %2d.%-25s %2d.%-25s \n",
+        printf(" %3d.%-25s %3d.%-25s %3d.%-25s \n",
           counter += 1, selected_class.meth_ods[counter - 1].name,
           counter += 1, selected_class.meth_ods[counter - 1].name,
           counter += 1, selected_class.meth_ods[counter - 1].name)
       end
       remainder.times do
-       printf(" %2d.%-25s", counter += 1, selected_class.meth_ods[counter - 1].name)
+       printf(" %3d.%-25s", counter += 1, selected_class.meth_ods[counter - 1].name)
       end
       printf("\n")
+      puts 'Enter the name of the method or exit to quit'
 
   end
 
@@ -178,7 +174,7 @@ class CommandLineInteface
   end
 
 
-    def bold_and_red(string)
+    def bold_and_green(string)
       string.colorize(:mode => :bold, :color => :green)
     end
 
